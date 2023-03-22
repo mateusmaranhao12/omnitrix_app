b3f6aade918aaaed24d8f9007b4895baa3d896b2
Mateus Maranhão <feradourada@gmail.com>
2023-03-22 16:27:58 -0300

    add AdicionarAlien.vue

--- src/views/AdicionarAlien.vue
+++ src/views/AdicionarAlien.vue
@@ -1,3 +1,20 @@
 <template>
-    <h1>Adicionar Alien</h1>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <h1 class="text-center mt-3">Adicionar Alien</h1>
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" placeholder="Nome do cliente" ref="nome" class="form-control" name="nome" />
+                </div>
+                <div class="mb-3">
+                    <label for="imagemAlien" class="form-label">Imagem do Alienígena</label>
+                    <input class="form-control" type="file" id="imagemAlien">
+                </div>
+                <div class="mb-3">
+                    <button class="btn btn-success float-end">Adicionar Alien</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
